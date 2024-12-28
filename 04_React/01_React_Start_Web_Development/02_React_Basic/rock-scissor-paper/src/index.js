c823443cd9e7a50a0984b7219a04fc3467d16c0d
hyeonjiroh <sonamu6509@naver.com>
2024-12-28 10:17:02 +0900

    Add rock hand icon

--- 04_React/01_React_Start_Web_Development/02_React_Basic/rock-scissor-paper/src/index.js
+++ 04_React/01_React_Start_Web_Development/02_React_Basic/rock-scissor-paper/src/index.js
@@ -1,32 +1,8 @@
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const WINS = {
-  rock: 'scissor',
-  scissor: 'paper',
-  paper: 'rock',
-};
-
-function getResult(left, right) {
-  if (WINS[left] === right) return '승리';
-  else if (left === WINS[right]) return '패배';
-  return '무승부';
-}
-
-function handleClick(e) {
-  console.log('가위바위보!');
-}
-
-const me = 'rock';
-const other = 'scissor';
-
 root.render(
-  <>
-    <h1 id="title">가위바위보</h1>
-    <h2>{getResult(me, other)}</h2>
-    <button className="hand" onClick={handleClick}>가위</button>
-    <button className="hand" onClick={handleClick}>바위</button>
-    <button className="hand" onClick={handleClick}>보</button>
-  </>
+  <App />
 );
