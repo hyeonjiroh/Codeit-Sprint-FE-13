b6a4984c3191281874035dd8e523329643ff28ae
hyeonjiroh <sonamu6509@naver.com>
2025-01-02 16:06:41 +0900

    sort로 배열 정렬하기

--- 04_React/02_React_Handle_Data/foodit/src/components/FoodList.js
+++ 04_React/02_React_Handle_Data/foodit/src/components/FoodList.js
@@ -1,4 +1,23 @@
-import FoodListItem from "./FoodListItem";
+import "./FoodList.css";
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+function FoodListItem({ item }) {
+  const { imgUrl, title, calorie, content, createdAt } = item;
+
+  return (
+    <div className="FoodListItem">
+      <img src={imgUrl} alt={title} />
+      <div>{title}</div>
+      <div>{calorie}</div>
+      <div>{content}</div>
+      <div>{formatDate(createdAt)}</div>
+    </div>
+  );
+}
 
 function FoodList({ items }) {
   return (
--- 04_React/02_React_Handle_Data/foodit/src/index.js
+++ 04_React/02_React_Handle_Data/foodit/src/index.js
@@ -1,8 +1,6 @@
-import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <App />
-);
+root.render(<App />);
