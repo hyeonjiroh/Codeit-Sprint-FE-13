f59fa7e3eaeadedd94842dc02eb2aedff8e060e0
hyeonjiroh <sonamu6509@naver.com>
2025-01-07 22:44:26 +0900

    글 작성하기

--- 04_React/02_React_Handle_Data/foodit/src/components/FoodForm.js
+++ 04_React/02_React_Handle_Data/foodit/src/components/FoodForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { createFood } from "../api";
 import FileInput from "./FileInput";
 
 function sanitize(type, value) {
@@ -11,17 +12,39 @@ function sanitize(type, value) {
   }
 }
 
-function FoodForm() {
-  const [values, setValues] = useState({
-    imgFile: null,
-    title: "",
-    calorie: 0,
-    content: "",
-  });
+const INITIAL_VALUES = {
+  imgFile: null,
+  title: "",
+  calorie: 0,
+  content: "",
+};
 
-  const handleSubmit = (e) => {
+function FoodForm({ onSubmitSuccess }) {
+  const [values, setValues] = useState(INITIAL_VALUES);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submittingError, setSubmittingError] = useState(null);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
+    const formData = new FormData();
+    formData.append("imgFile", values.imgFile);
+    formData.append("title", values.title);
+    formData.append("calorie", values.calorie);
+    formData.append("content", values.content);
+    let result;
+    try {
+      setSubmittingError(null);
+      setIsSubmitting(true);
+      result = await createFood(formData);
+    } catch (error) {
+      setSubmittingError(error);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
+    const { food } = result;
+    onSubmitSuccess(food);
+    setValues(INITIAL_VALUES);
   };
 
   const handleChange = (name, value) => {
@@ -55,7 +78,10 @@ function FoodForm() {
         value={values.content}
         onChange={handleInputChange}
       />
-      <button type="submit">확인</button>
+      <button disabled={isSubmitting} type="submit">
+        확인
+      </button>
+      {submittingError && <div>{submittingError.message}</div>}
     </form>
   );
 }
