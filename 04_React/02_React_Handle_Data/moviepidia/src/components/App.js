8cb5bffc46be5cfac85aa21cec094749de244130
hyeonjiroh <sonamu6509@naver.com>
2025-01-10 17:51:08 +0900

    Upload

---
+++ 04_React/02_React_Handle_Data/moviepidia/src/components/App.js
@@ -0,0 +1,99 @@
+import { useCallback, useEffect, useState } from "react";
+import ReviewList from "./ReviewList";
+import ReviewForm from "./ReviewForm";
+import { createReview, deleteReview, getReviews, updateReview } from "../api";
+import useAsync from "../hooks/useAsync";
+import { LocaleProvider } from "../contexts/LocaleContext";
+import LocaleSelect from "./LocaleSelect";
+
+const LIMIT = 6;
+
+function App() {
+  const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+  const [isLoading, loadingError, getReviewsAsync] = useAsync(getReviews);
+  const [items, setItems] = useState([]);
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+
+  const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = async (id) => {
+    const result = await deleteReview(id);
+    if (!result) return;
+
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  const handleLoad = useCallback(
+    async (options) => {
+      const result = await getReviewsAsync(options);
+      if (!result) return;
+
+      const { paging, reviews } = result;
+      if (options.offset === 0) {
+        setItems(reviews);
+      } else {
+        setItems((prevItems) => [...prevItems, ...reviews]);
+      }
+      setOffset(options.offset + options.limit);
+      setHasNext(paging.hasNext);
+    },
+    [getReviewsAsync]
+  );
+
+  const handleLoadMore = async () => {
+    await handleLoad({ order, offset, limit: LIMIT });
+  };
+
+  const handleCreateSuccess = (review) => {
+    setItems((prevItems) => [review, ...prevItems]);
+  };
+
+  const handleUpdateSuccess = (review) => {
+    setItems((prevItems) => {
+      const splitIdx = prevItems.findIndex((item) => item.id === review.id);
+      return [
+        ...prevItems.slice(0, splitIdx),
+        review,
+        ...prevItems.slice(splitIdx + 1),
+      ];
+    });
+  };
+
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order, handleLoad]);
+
+  return (
+    <LocaleProvider defaultValue="ko">
+      <div>
+        <LocaleSelect />
+        <div>
+          <button onClick={handleNewestClick}>최신순</button>
+          <button onClick={handleBestClick}>베스트순</button>
+        </div>
+        <ReviewForm
+          onSubmit={createReview}
+          onSubmitSuccess={handleCreateSuccess}
+        />
+        <ReviewList
+          items={sortedItems}
+          onDelete={handleDelete}
+          onUpdate={updateReview}
+          onUpdateSuccess={handleUpdateSuccess}
+        />
+        {hasNext && (
+          <button disabled={isLoading} onClick={handleLoadMore}>
+            더 보기
+          </button>
+        )}
+        {loadingError?.message && <span>{loadingError.message}</span>}
+      </div>
+    </LocaleProvider>
+  );
+}
+
+export default App;
---
+++ 04_React/02_React_Handle_Data/moviepidia/src/components/ReviewForm.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import useAsync from "../hooks/useAsync";
+import useTranslate from "../hooks/useTranslate";
+import FileInput from "./FileInput";
+import RatingInput from "./RatingInput";
+import "./ReviewForm.css";
+
+const INITIAL_VALUES = {
+  title: "",
+  rating: 0,
+  content: "",
+  imgFile: null,
+};
+
+function ReviewForm({
+  initialValues = INITIAL_VALUES,
+  initialPreview,
+  onCancel,
+  onSubmit,
+  onSubmitSuccess,
+}) {
+  const t = useTranslate();
+  const [values, setValues] = useState(initialValues);
+  const [isSubmitting, submittingError, onSubmitAsync] = useAsync(onSubmit);
+
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", values.title);
+    formData.append("rating", values.rating);
+    formData.append("content", values.content);
+    formData.append("imgFile", values.imgFile);
+
+    const result = await onSubmitAsync(formData);
+    if (!result) return;
+
+    const { review } = result;
+    setValues(INITIAL_VALUES);
+    onSubmitSuccess(review);
+  };
+
+  return (
+    <form className="ReviewForm" onSubmit={handleSubmit}>
+      <FileInput
+        name="imgFile"
+        value={values.imgFile}
+        initialPreview={initialPreview}
+        onChange={handleChange}
+      />
+      <input name="title" value={values.title} onChange={handleInputChange} />
+      <RatingInput
+        name="rating"
+        value={values.rating}
+        onChange={handleChange}
+      />
+      <textarea
+        name="content"
+        value={values.content}
+        onChange={handleInputChange}
+      />
+      {onCancel && <button onClick={onCancel}>{t("cancel button")}</button>}
+      <button disabled={isSubmitting} type="submit">
+        {t("confirm button")}
+      </button>
+      {submittingError && <div>{submittingError.message}</div>}
+    </form>
+  );
+}
+
+export default ReviewForm;
