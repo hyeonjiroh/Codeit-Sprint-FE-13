574b6a54856a00db15617e172b3b574d9e5161e9
hyeonjiroh <sonamu6509@naver.com>
2025-01-20 19:40:53 +0900

    Nesting

--- 04_React/04_Styled_Components/hello-styled/src/Button.js
+++ 04_React/04_Styled_Components/hello-styled/src/Button.js
@@ -1,10 +1,34 @@
 import styled from "styled-components";
+import nailImg from "./nail.png";
 
-const Button = styled.button`
+const Icon = styled.img`
+  width: 16px;
+  height: 16px;
+`;
+
+const StyledButton = styled.button`
   background-color: #6750a4;
   border: none;
   color: #ffffff;
   padding: 16px;
+
+  & ${Icon} {
+    margin-right: 4px;
+  }
+
+  &:hover,
+  &:active {
+    background-color: #463770;
+  }
 `;
+
+function Button({ children, ...buttonProps }) {
+  return (
+    <StyledButton {...buttonProps}>
+      <Icon src={nailImg} alt="nail icon" />
+      {children}
+    </StyledButton>
+  );
+}
 
 export default Button;
