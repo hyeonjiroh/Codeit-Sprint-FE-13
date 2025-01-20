fb96844ddc4bfadec63ece64ed40413666feb845
hyeonjiroh <sonamu6509@naver.com>
2025-01-20 19:52:01 +0900

    Input 컴포넌트: 배치하기

--- 04_React/04_Styled_Components/hello-styled/src/App.js
+++ 04_React/04_Styled_Components/hello-styled/src/App.js
@@ -1,10 +1,27 @@
+import styled from "styled-components";
 import Input from "./Input";
+
+const Container = styled.div`
+  margin: 0 auto;
+  width: 400px;
+
+  ${Input} {
+    box-sizing: border-box;
+    display: block;
+    margin: 8px 0 16px;
+    width: 100%;
+  }
+`;
 
 function App() {
   return (
-    <div>
-      <Input />
-    </div>
+    <Container>
+      <h1>로그인</h1>
+      <label htmlFor="email">Email</label>
+      <Input type="email" id="email" placeholder="styled@codeit.kr" />
+      <label htmlFor="password">Password</label>
+      <Input type="password" id="password" placeholder="비밀번호" />
+    </Container>
   );
 }
 
