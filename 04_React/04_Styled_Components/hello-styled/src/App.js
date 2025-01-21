8b7bf6506a056a117fcb4bd6742831330e87b2a7
hyeonjiroh <sonamu6509@naver.com>
2025-01-21 11:20:58 +0900

    Input 컴포넌트: 속성 추가하기

--- 04_React/04_Styled_Components/hello-styled/src/App.js
+++ 04_React/04_Styled_Components/hello-styled/src/App.js
@@ -2,25 +2,22 @@ import styled from "styled-components";
 import Input from "./Input";
 
 const Container = styled.div`
-  margin: 0 auto;
-  width: 400px;
-
   ${Input} {
-    box-sizing: border-box;
-    display: block;
-    margin: 8px 0 16px;
-    width: 100%;
+    margin: 8px;
   }
 `;
 
 function App() {
   return (
     <Container>
-      <h1>로그인</h1>
-      <label htmlFor="email">Email</label>
-      <Input type="email" id="email" placeholder="styled@codeit.kr" />
-      <label htmlFor="password">Password</label>
-      <Input type="password" id="password" placeholder="비밀번호" />
+      <h2>Size</h2>
+      <Input size="small" />
+      <Input size="medium" />
+      <Input size="large" />
+      <h2>Round</h2>
+      <Input round />
+      <h2>Error</h2>
+      <Input error />
     </Container>
   );
 }
--- 04_React/04_Styled_Components/hello-styled/src/Input.js
+++ 04_React/04_Styled_Components/hello-styled/src/Input.js
@@ -1,13 +1,20 @@
 import styled from "styled-components";
 
+const SIZES = {
+  large: 24,
+  medium: 20,
+  small: 16,
+};
+
 const Input = styled.input`
-  border: 2px solid #eeeeee;
-  border-radius: 4px;
+  font-size: ${({ size }) => SIZES[size] ?? SIZES["medium"]}px;
+  border: 2px solid ${({ error }) => (error ? `#f44336` : `#eeeeee`)};
+  border-radius: ${({ round }) => (round ? `9999px` : `4px`)};
   outline: none;
   padding: 16px;
 
   &:focus {
-    border-color: #7760b4;
+    border-color: ${({ error }) => (error ? `#f44336` : `#7760b4`)};
   }
 `;
 
