d812f28a3dbeb1e527c4d6df09d9a51be55cb836
hyeonjiroh <sonamu6509@naver.com>
2025-02-13 20:06:21 +0900

    게임 로직 타입스크립트로 옮기기

--- 05_TypeScript/01_TypeScript_Basic/main.js
+++ 05_TypeScript/01_TypeScript_Basic/main.js
@@ -1,15 +1,10 @@
 "use strict";
-let monsterName;
-let level;
-let hasGold;
-let skill;
-let area;
-monsterName = "코드잇 고블린";
-level = 255;
-hasGold = false;
-skill = undefined;
-area = null;
-console.log(`${monsterName}의 레벨은 ${level}이고,\n` +
-    `${hasGold ? "해치우면 골드를 얻는" : "해치워도 골드를 주지 않는"} 몬스터입니다.\n` +
-    `${skill ? `가진 능력은 ${skill} 이고 \n` : ""}` +
-    `${area ? `출현 지역은 ${area} 입니다.` : ""}`);
+const current = [0, 0];
+const target = [4, 5];
+const dx = target[0] - current[0];
+const dy = target[1] - current[1];
+console.log(`오른쪽으로 ${dx} 위쪽으로 ${dy} 만큼 이동!`);
+const items = [];
+items.push("갑옷");
+items.push("빨간 물약");
+console.log(`${items.join(", ")}을/를 획득했다!`);
