b04e386905338693c74ea4f0241ff4d849493ed1
hyeonjiroh <sonamu6509@naver.com>
2025-02-14 20:01:35 +0900

    Monster 타입 재사용하기

--- 05_TypeScript/01_TypeScript_Basic/main.js
+++ 05_TypeScript/01_TypeScript_Basic/main.js
@@ -1,10 +1,14 @@
 "use strict";
-var Job;
-(function (Job) {
-    Job["Knight"] = "Knight";
-    Job["Archer"] = "Archer";
-    Job["Mage"] = "Mage";
-    Job["Priest"] = "Priest";
-    Job["Thief"] = "Thief";
-})(Job || (Job = {}));
-console.log(Job);
+let monster = {
+    id: "g001",
+    name: "고블린",
+    level: 22,
+    skills: ["태권도", "특공무술"],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+};
+console.log(`${monster.name}(${monster.id})의 레벨은 ${monster.level}이고,\n` +
+    `${monster.hasGold ? "해치우면 골드를 얻는" : "해치워도 골드를 주지 않는"} 몬스터입니다.\n` +
+    `${monster.skills.length > 0
+        ? `가진 능력은 ${monster.skills.join(", ")}입니다.`
+        : ""}`);
