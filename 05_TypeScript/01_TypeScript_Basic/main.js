0fd0059c6bf6169fd281c77de8a77a0a51822928
hyeonjiroh <sonamu6509@naver.com>
2025-02-16 01:01:28 +0900

    tsconfig.json 옵션

--- 05_TypeScript/01_TypeScript_Basic/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const stock = new Map();
-stock.set("g001", 1);
-stock.set("g002", 2);
-console.log(stock.get("g001"));
-console.log(stock.get("g002"));
--- 05_TypeScript/01_TypeScript_Basic/main.ts
+++
@@ -1,5 +0,0 @@
-const stock = new Map<string, number>();
-stock.set("g001", 1);
-stock.set("g002", 2);
-console.log(stock.get("g001"));
-console.log(stock.get("g002"));
