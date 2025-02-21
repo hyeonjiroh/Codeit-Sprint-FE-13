cd553d355bfe4736f5178d3890bcff9c8cd43b44
hyeonjiroh <sonamu6509@naver.com>
2025-02-21 15:29:50 +0900

    페이지 이동

--- 06_Next/01_Next_Make_Web-Site/watchit/pages/movies/[id].js
+++ 06_Next/01_Next_Make_Web-Site/watchit/pages/movies/[id].js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 
 export default function Movie() {
   const router = useRouter();
-  const { id } = router.query;
+  const id = router.query["id"];
 
   return <>Movie 페이지 #{id}</>;
 }
