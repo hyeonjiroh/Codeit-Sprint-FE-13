76fee951dafeb3a6ce18b70f8a6e8e94f5577259
hyeonjiroh <sonamu6509@naver.com>
2025-02-26 16:17:51 +0900

    커스텀 App

--- 06_Next/01_Next_Make_Web-Site/watchit/pages/search.js
+++ 06_Next/01_Next_Make_Web-Site/watchit/pages/search.js
@@ -3,8 +3,6 @@ import { useRouter } from "next/router";
 import MovieList from "@/components/MovieList";
 import SearchForm from "@/components/SearchForm";
 import styles from "@/styles/Search.module.css";
-import Header from "@/components/Header";
-import Container from "@/components/Container";
 import axios from "@/lib/axios";
 
 export default function Search() {
@@ -24,14 +22,11 @@ export default function Search() {
 
   return (
     <>
-      <Header />
-      <Container page>
-        <SearchForm initialValue={q} />
-        <h2 className={styles.title}>
-          <span className={styles.keyword}>{q}</span> 검색 결과
-        </h2>
-        <MovieList movies={movies} />
-      </Container>
+      <SearchForm initialValue={q} />
+      <h2 className={styles.title}>
+        <span className={styles.keyword}>{q}</span> 검색 결과
+      </h2>
+      <MovieList movies={movies} />
     </>
   );
 }
