19ca3cac100059cfa94bb88c9a81fd4153f60035
hyeonjiroh <sonamu6509@naver.com>
2025-03-04 16:49:01 +0900

    리스폰스 다루기

--- 06_Next/02_Next_Make_API/shortit/pages/api/short-links/[id].js
+++ 06_Next/02_Next_Make_API/shortit/pages/api/short-links/[id].js
@@ -1,3 +1,15 @@
 export default function handler(req, res) {
-  res.send(req.method);
+  switch (req.method) {
+    case "PATCH":
+      res.send("ShortLink 수정");
+      break;
+
+    case "GET":
+      res.send("ShortLink 조회");
+      break;
+
+    default:
+      res.send();
+      break;
+  }
 }
