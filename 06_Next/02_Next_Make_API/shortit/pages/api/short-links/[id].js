0005fdfcfa38d66368c193afe8a3dc4918a61ca2
hyeonjiroh <sonamu6509@naver.com>
2025-03-05 20:53:11 +0900

    도큐먼트 생성, 조회하기

--- 06_Next/02_Next_Make_API/shortit/pages/api/short-links/[id].js
+++ 06_Next/02_Next_Make_API/shortit/pages/api/short-links/[id].js
@@ -1,11 +1,18 @@
-export default function handler(req, res) {
+import dbConnect from "@/db/dbConnect";
+import ShortLink from "@/db/models/ShortLink";
+
+export default async function handler(req, res) {
+  await dbConnect();
+  const { id } = req.query;
+
   switch (req.method) {
     case "PATCH":
       res.send("ShortLink 수정");
       break;
 
     case "GET":
-      res.send("ShortLink 조회");
+      const shortLink = await ShortLink.findById(id);
+      res.send(shortLink);
       break;
 
     default:
